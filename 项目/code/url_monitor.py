dd871f040c661f9801fb427905f20b3bcdb05d9d
sageskr <sageskr@163.com>
2015-02-13 15:02:25 +0800

    增加创建库和表的SQ

--- 项目/code/url_monitor.py
+++ 项目/code/url_monitor.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 __author__ = 'kairong'
+
 import ConfigParser
+import MySQLdb
+
 cf  = ConfigParser.ConfigParser()
 cf.read("./local_config")
-print cf.get('DB','db')
+
+def get_args(main, name):
+    return cf.get(main, name)
+
+db_host = get_args("DB", "db_host")
+db_user = get_args("DB", "username")
+db_default = get_args("DB", "db")
