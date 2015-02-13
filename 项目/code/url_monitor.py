14aab79b5aa579f43d1884eed4fdc2a6dfa91e8b
sageskr <sageskr@163.com>
2015-02-13 15:22:49 +0800

    提交测试链接方法

--- 项目/code/url_monitor.py
+++ 项目/code/url_monitor.py
@@ -4,6 +4,7 @@ __author__ = 'kairong'
 
 import ConfigParser
 import MySQLdb
+import requests
 
 cf  = ConfigParser.ConfigParser()
 cf.read("./local_config")
@@ -11,6 +12,14 @@ cf.read("./local_config")
 def get_args(main, name):
     return cf.get(main, name)
 
-db_host = get_args("DB", "db_host")
+#初始化各种参数
+db_hostname = str(get_args("DB", "db_host"))
 db_user = get_args("DB", "username")
+db_pass = get_args("DB", "passwd")
 db_default = get_args("DB", "db")
+#建立mysql游标
+conn = MySQLdb.connect(host='localhost', user=db_user, passwd='test', db='url_mon')
+cur = conn.cursor()
+
+cur.execute("show tables;")
+print cur.fetchall()
