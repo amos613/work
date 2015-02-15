b1817de681b41ea3f44e7622ad7884e28f78d5bf
sageskr <sageskr@163.com>
2015-02-15 14:51:59 +0800

    增加上报异常数大于50%时，报警

--- 项目/code/mon_alarm.py
+++ 项目/code/mon_alarm.py
@@ -11,15 +11,32 @@ def alarm(id):
 
 def check_status(id):
     """根据ID查询报警阈值，以及现在的状态值"""
+    re_dict = {"status_0":0}
+    sum_c = 0
     conn = MySQLdb.connect(host=db_hostname, user=db_user, passwd=db_pass, db='url_mon',charset='utf8')
     cur = conn.cursor()
     sql_get_id_status = "select C.status_code,A.fail_time from status_code AS C,montior_url AS A where C.ID = A.ID and C.ID = %d;" %(id)
     cur.execute(sql_get_id_status)
-    c_status, a_status = cur.fetchone()
-    if c_status >= a_status:
-        alarm(id)
+    status_all = cur.fetchall()
+    print id,
+    for c_status, a_status in status_all:
+        sum_c += c_status
+        d_key = "status_%d" % (c_status)
+        re_dict[d_key] = re_dict.get(d_key,0) + 1
+    if sum_c == 0:
+        print "ID %d is ok" % (id,)
     else:
-        print "ID %d is ok" %(id)
+        sum_v = 0
+        for v in re_dict.values():
+            sum_v += v
+        if re_dict["status_0"] / float(sum_v) < 0.5:
+            print "ID %d is error" % (id,)
+
+
+    # if c_status >= a_status:
+    #     alarm(id)
+    # else:
+    #     print "ID %d is ok" %(id)
 
 def get_args(main, name):
     """获取配置"""
@@ -27,7 +44,6 @@ def get_args(main, name):
 
 def main():
     '''获取所有现在的监控列表的ID,根据ID触发监控'''
-    print time.time()
     conn = MySQLdb.connect(host=db_hostname, user=db_user, passwd=db_pass, db='url_mon',charset='utf8')
     cur = conn.cursor()
     sql_get_id = "select ID from montior_url;"
@@ -36,10 +52,10 @@ def main():
         line = cur.fetchone()
         if not line:
             break
+        #check_status(line[0])
         Process(target=check_status,args=(line[0],)).start()
-    print time.time()
 
-cf  = ConfigParser.ConfigParser()
+cf = ConfigParser.ConfigParser()
 cf.read("./local_config")
 db_hostname = get_args("DB", "db_host")
 db_user = get_args("DB", "username")
